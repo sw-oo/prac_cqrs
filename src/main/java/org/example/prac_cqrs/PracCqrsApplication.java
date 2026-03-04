4452d4c1a8dd7d8217c6c530fe6f60ddaba225b7
swoo <kokodd1234@gmail.com>
2026-03-04 14:08:43 +0900

    [Init] 프로젝트 초기화

---
+++ src/main/java/org/example/prac_cqrs/PracCqrsApplication.java
@@ -0,0 +1,13 @@
+package org.example.prac_cqrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracCqrsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracCqrsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/prac_cqrs/PracCqrsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.prac_cqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracCqrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
